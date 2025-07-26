6d47b166bdbe6b3bdbcc750414b525b6e7b36a85
Daniel Desktop <danieladiazop@gmail.com>
2025-07-26 00:41:34 -0700

    Worked on Task 1-4

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,14 +1,38 @@
 import csv
+import traceback
+
 
 file_path = "C:/Users/danie/CTD/Python/python_homework/csv/employees.csv"
 
 def read_employees():
-    emptyDict = {}
+    result = {}
+    rows = []
+
     try:
-        with open(file_path, "w") as file:
-            write = csv.writer(file)
-            for row in employees:
-                writer.writerow(row)
-            print(f"csv file {file_path} was created")
-        except FileExistsError:
-            print("That file already exists!")
+        with open(file_path, "r") as file:
+            reader = csv.reader(file)
+            for i, row in enumerate(reader):
+                if i == 0:
+                    result["fields"] = row
+                else:
+                    row.append(row)
+            result["rows"] = rows
+    except Exception as e:
+        trace_back = traceback.extract_tb(e.__traceback__)
+        stack_trace = []
+        for trace in trace_back:
+            stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+        print(f"Exception type: {type(e).__name__}")
+        message = str(e)
+        if message:
+            print(f"Exception message: {message}")
+        print(f"Stack trace: {stack_trace}")
+
+    return result    
+
+def column_index(string):
+    employee_id_column = employees["fields"].index(string) 
+    return employee_id_column
+
+def first_name(rowNumber):
+     
--- assignment2/diary.py
+++ assignment2/diary.py
@@ -1,18 +1,22 @@
 import traceback
+
 try:
-    with open('diary.txt', 'a') as file:
-        prompt = "What did you do today?"
-        while True: 
+    with open("diary.txt", "a") as file:
+        prompt = "What did you do today? "
+        while True:
             txt_data = input(prompt)
             file.write(txt_data + "\n")
-            prompt = "What else"
-            if txt_data == "done for now":
+
+            if txt_data.strip().lower() == "done for now":
                 break
+
+            prompt = "What else? "
 except Exception as e:
+    import traceback
     trace_back = traceback.extract_tb(e.__traceback__)
     stack_trace = list()
     for trace in trace_back:
-      stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+        stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
     print(f"Exception type: {type(e).__name__}")
     message = str(e)
     if message:
