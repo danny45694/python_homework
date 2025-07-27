a91a1251794ee191aed165ae00cacaf3e2ee26af
Daniel Desktop <danieladiazop@gmail.com>
2025-07-27 01:58:44 -0700

    Worked on lesson2 more

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,22 +1,22 @@
 import csv
 import traceback
 
-
+#Task 2: Read a CSV File
 file_path = "C:/Users/danie/CTD/Python/python_homework/csv/employees.csv"
 
 def read_employees():
-    result = {}
+    employee_data = {}
     rows = []
 
     try:
-        with open(file_path, "r") as file:
-            reader = csv.reader(file)
-            for i, row in enumerate(reader):
+        with open(file_path, 'r') as data_file:
+            csv_data = csv.reader(data_file)
+            for i, row in enumerate(csv_data):
                 if i == 0:
-                    result["fields"] = row
+                    employee_data['fields'] = row
                 else:
-                    row.append(row)
-            result["rows"] = rows
+                    rows.append(row)
+            employee_data["rows"] = rows
     except Exception as e:
         trace_back = traceback.extract_tb(e.__traceback__)
         stack_trace = []
@@ -28,14 +28,43 @@ def read_employees():
             print(f"Exception message: {message}")
         print(f"Stack trace: {stack_trace}")
 
-    return result    
+    return employee_data
 
+employees = read_employees()
+
+
+#Task 3: Find the Column Index
 def column_index(string):
-    employee_id_column = employees["fields"].index(string) 
-    return employee_id_column
+    return employees["fields"].index(string) 
 
-print(column_index("employee_id"))
+employee_id_column = column_index("employee_id")
 
 
-# def first_name(rowNumber):
-     
+
+#Task 4: Find the Employee First Name
+def first_name(rowNumber):
+    columnNumber = column_index("first_name")
+    employee_name = employees["rows"][rowNumber]
+    return employee_name[columnNumber]
+
+
+#Task 5: Find the Employee: a Function in a Function
+
+def employee_find(employee_id):
+    def employee_match(row):
+        return int(row[employee_id_column]) == employee_id
+    matches=list(filter(employee_match, employees["rows"]))
+    return matches
+
+#Task 6: Find the Employee with a Lambda
+def employee_find_2(employee_id):
+    matches = list(filter(lambda row: int(row[employee_id_column]) == employee_id , employees["rows"])) 
+    return matches
+
+#Task 7: Sort the Rows by last_name Using a Lambda
+def sort_by_last_name():
+    last_names = column_index("last_name")
+    sorted_names = employees["rows"].sort(key=lambda row: row[last_names])
+    return sorted_names
+
+print(sort_by_last_name())
