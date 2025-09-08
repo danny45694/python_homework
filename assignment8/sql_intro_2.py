f32860fbb5d8a7a744c3aaab6414c960a42bb009
Daniel Desktop <danieladiazop@gmail.com>
2025-09-07 18:48:43 -0700

    got past a roadblock

--- assignment8/sql_intro_2.py
+++ assignment8/sql_intro_2.py
@@ -3,69 +3,71 @@ import sqlite3
 
 
 
-#Create Database
+# ---------------- DB SETUP ----------------
 try:
-    with sqlite3.connect("../db/magazines.db") as conn:
+    with sqlite3.connect("../db/lesson.db") as conn:
         conn.execute("PRAGMA foreign_keys = 1")
         cursor = conn.cursor()
 
         cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS products (
+                    product_id INTEGER PRIMARY KEY,
+                    product_name TEXT UNIQUE NOT NULL,
+                    price REAL NOT NULL)
+                    """)
+
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS line_items (
                     line_item_id INTEGER PRIMARY KEY,
-                    quantity INTEGER,
-                    product_id INTEGER,
-                    product_name TEXT NOT NULL,
-                    price INTEGER,
-                    FOREIGN KEY (product_id) REFERENCES products (product_id),
-                    FOREIGN KEY (product_name) REFERENCES products (product_name)
+                    quantity INTEGER NOT NULL,
+                    product_id INTEGER NOT NULL,
+                    FOREIGN KEY (product_id) REFERENCES products (product_id)
             )
         """)
 
-        cursor.execute("""
-                    CREATE TABLE IF NOT EXISTS products (
-                    product_id INTEGER PRIMARY KEY,
-                    product_name TEXT NOT NULL,
-                    price INTEGER)
-                    """)
+        
                     
       
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
-#Functions to add data
-def add_line_items(cursor, line_item_id, quantity, product_id, product_name, price):
-    try:
-        cursor.execute("INSERT INTO line_items (line_item_id, quantity, product_id, product_name, price) VALUES (?,?,?,?,?)", (line_item_id, quantity, product_id, product_name, price))
-    except sqlite3.IntegrityError:
-        print(f"{product_name} is already in the database.")
-
-
+# ---------------- ADD FUNCTIONS ----------------
 def add_products(cursor, product_id, product_name, price):
     try:
         cursor.execute("INSERT INTO products (product_id, product_name, price) VALUES (?,?,?)", (product_id, product_name, price))
     except sqlite3.IntegrityError:
         print(f"{product_name} is already in the database.")
 
+def add_line_items(cursor, line_item_id, quantity, product_id):
+    try:
+        cursor.execute("INSERT INTO line_items (line_item_id, quantity, product_id) VALUES (?,?,?)", (line_item_id, quantity, product_id))
+    except sqlite3.IntegrityError:
+        print(f"{line_item_id} is already in the database.")
 
 
 
 
-with sqlite3.connect("../db/sql_intro_2.py") as conn:
-    conn.execute("PRAGMA foreign_keys = 1")
+
+
+
+
+with sqlite3.connect("../db/lesson.db") as conn:
     cursor = conn.cursor()
 
-    #ADD DATA
-
-    add_line_items(cursor, 1, 1, 1, 'milk', 3.99)
-    add_line_items(cursor, 2, 2, 2, 'eggs', 2.99)
-    add_line_items(cursor, 3, 1, 3, 'coffee', 4.65)
-    add_line_items(cursor, 4, 1, 4, 'rice', 1.99)
-
+# ---------------- DATA ----------------
 
     add_products(cursor, 1, 'milk', 3.99)
     add_products(cursor, 2, 'eggs', 2.99)
     add_products(cursor, 3, 'coffee', 4.65)
     add_products(cursor, 4, 'rice', 1.99)
+
+    add_line_items(cursor, 1, 1, 1)
+    add_line_items(cursor, 2, 2, 2)
+    add_line_items(cursor, 3, 1, 3)
+    add_line_items(cursor, 4, 1, 4)
+
+
+
 
     conn.commit()
 
@@ -76,11 +78,12 @@ with sqlite3.connect("../db/sql_intro_2.py") as conn:
 
 
 sql_statement = """
-        SELECT line_item_id, quantity, product_id, product_name, price
+        SELECT line_items.line_item_id, line_items.quantity, products.product_id, products.product_name, products.price
         FROM line_items
-        LEFT JOIN products ON line_items.product_id = products.product_id
+        JOIN products ON line_items.product_id = products.product_id
         """
 
-#Read into dataframe
+#-------------- Read into dataframe --------------
 df = pd.read_sql_query(sql_statement, conn)
 
+print(df)
