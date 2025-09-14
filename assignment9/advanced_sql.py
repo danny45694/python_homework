01ecea94a0a4a6a67cd127aa181aff88fa213628
Daniel Desktop <danieladiazop@gmail.com>
2025-09-14 00:25:44 -0700

    Finished task 2

--- assignment9/advanced_sql.py
+++ assignment9/advanced_sql.py
@@ -3,23 +3,46 @@ import sqlite3
 with sqlite3.connect("../db/lesson.db") as conn:
     cursor = conn.cursor()
 
-# Join the tables
-total_price =  """ WITH joined AS (
-    SELECT order_id, products, line_items
-    FROM orders
-    JOIN line_items ON orders.order_id = line_items.order_id
-    JOIN products ON products.product_id = line_items.product_id
-    GROUP BY order_id
-),
-    add_tables AS (
-    SELECT order_id, SUM(products.price * line_items.quantity)
-    FROM joined_tables
-    ORDER BY order_id
-    LIMIT 5
-    )
+# Task 1---------------------------------------------------------------
+total_price =  """ 
+    SELECT o.order_id, li.quantity, p.price, p.price * li.quantity AS line_total
+    FROM orders AS o
+    JOIN line_items AS li ON o.order_id = li.order_id
+    JOIN products AS p ON p.product_id = li.product_id
+    GROUP BY o.order_id
+    ORDER BY o.order_id ASC
+    LIMIT 5;
     """
+    
+
+
+    # Return total price using AS total_price and customer_id AS customer_id_b. Return customer_id and total_price in subquery. Main statement, left join the customer table with the results of the subquery, using ON customer_id = customer_id_b
+#------------------------------Task2-------------------------------
+average_price = """
+SELECT
+  c.customer_name,
+  AVG(t.total_price) AS average_total_price
+FROM customers AS c
+LEFT JOIN (
+  SELECT
+    o.order_id,
+    o.customer_id AS customer_id_b,
+    SUM(li.quantity * p.price) AS total_price
+  FROM orders AS o
+  JOIN line_items AS li ON li.order_id = o.order_id
+  JOIN products  AS p  ON p.product_id = li.product_id
+  GROUP BY o.order_id, o.customer_id
+) AS t
+  ON c.customer_id = t.customer_id_b
+GROUP BY c.customer_id, c.customer_name
+ORDER BY c.customer_id ASC
+LIMIT 5;
+"""
 
 cursor.execute(total_price)
 results = cursor.fetchall()
 print(results)
 conn.close()
+
+
+
